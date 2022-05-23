68103672b77cc956fa075e8e9c6c11479f6272a2
Andres Guevara <andres-guevara1@upc.edu.co>
2022-05-22 22:45:26 -0500

    Carga inicial

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,26 @@
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 import { BrowserModule } from '@angular/platform-browser';
+import { Routes } from '@angular/router';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ErroresComponent } from './errores/errores.component';
+
+const appRoutes: Routes =[
+  { path: '', component: AppComponent},
+  { path: '**', component: ErroresComponent}
+]
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ErroresComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
